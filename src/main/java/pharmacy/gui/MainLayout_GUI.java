38846bca57dd9c3aa1e45b2f631ff42f56d47a5f
Nooba <dpnguyen261204@gmail.com>
2024-10-22 17:12:42 +0700

    handle features of medicines frame

--- src/main/java/pharmacy/gui/MainLayout_GUI.java
+++ src/main/java/pharmacy/gui/MainLayout_GUI.java
@@ -80,9 +80,9 @@ public class MainLayout_GUI extends Application {
         screenWidth = bounds.getWidth();
         screenHeight = bounds.getHeight();
 
-        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-        primaryStage.setWidth(screenBounds.getWidth());
-        primaryStage.setHeight(screenBounds.getHeight());
+        // Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        // primaryStage.setWidth(screenBounds.getWidth());
+        // primaryStage.setHeight(screenBounds.getHeight());
 
         // set header layout
         headerRightBox.setMinWidth(screenWidth - (menuBtnPane.getPrefWidth() + logoLabel.getPrefWidth()));
